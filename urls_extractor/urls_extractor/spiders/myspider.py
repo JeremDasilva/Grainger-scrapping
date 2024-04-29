4c983a67d849e40d6e2bf0b4564d428aedc3f0d3
Jeremy DA SILVA <136356801+JeremDasilva@users.noreply.github.com>
2024-04-29 16:26:21 +0200

    Json to dataviz
    Tries on reading json files and taking them to data viz

--- urls_extractor/urls_extractor/spiders/myspider.py
+++ urls_extractor/urls_extractor/spiders/myspider.py
@@ -3,12 +3,12 @@ import scrapy
 class MyspiderSpider(scrapy.Spider):
     name = "myspider"
     allowed_domains = ["www.grainger.com"]
-    start_urls = ["https://www.grainger.com/category/lighting/light-bulbs-lamps?categoryIndex=1"]
+    start_urls = ["https://www.grainger.com/category/lighting/light-bulbs-lamps?categoryIndex=1"] #Starting point 'deep' in order to make the testing faster
 
     def parse(self, response):
         links = response.css('a.BQOwdV')
         
-        if len(links) > 0:
+        if len(links) > 0: #We look on the page and if we find href. If yes we keep going deeper in the website, if not we yield the url in a json file called urls (see settings)
             for link in links:
                 absolute_next_page_url = 'https://www.grainger.com' + link.attrib.get('href')
                 yield scrapy.Request(absolute_next_page_url, callback=self.parse)
