3e41fc177800c9f84969c472450171fae73581a0
Jeremy DA SILVA <136356801+JeremDasilva@users.noreply.github.com>
2024-05-21 17:47:41 +0200

    Web app dropdown and request

--- web_app.py
+++ web_app.py
@@ -1,5 +1,5 @@
-#Importing libraries:
-import streamlit as st   
+# Importing libraries:
+import streamlit as st
 import json
 import os
 import re
@@ -8,17 +8,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from web_app_functions import *
 
-#Page formatting :
+# Page formatting :
 st.title('Scrapping user page')
 st.sidebar.title('Menu')
 
 if st.sidebar.checkbox('Run scrapping'):
-    
+
     st.markdown('## Scrapping setting')
-        
-    st.write("afficher les main category qu'on veut scrap, les cocher et faire un bouton go")
+
+    st.write(
+        "afficher les main category qu'on veut scrap, les cocher et faire un bouton go")
     st.write("Which categories do you want to scrap?")
-    
+
     with open('urls_extractor/urls_extractor/spiders/urls.json', 'r') as file:
         categories_to_get = json.load(file)
     categories = []
@@ -27,26 +28,26 @@ if st.sidebar.checkbox('Run scrapping'):
             category = value.split('/')[4]
             categories.append(category)
     categories = list(set(categories))
-    
+
     for cat in categories:
         st.checkbox(cat)
-        
+
     if st.button("Scrap"):
         st.markdown('Start scraping on selected categories')
-        
+
     if st.button("Scrap all"):
         st.markdown('Scrapping on going')
         with open('urls_extractor/urls_extractor/spiders/urls.json', 'r') as file:
             datas = json.load(file)
-    
+
         urls_to_scrap = []
 
-        for data in datas :
+        for data in datas:
             for key, value in data.items():
                 urls_to_scrap.append(value)
-                
+
         u = urls_to_scrap[0:3]
-        
+
         results = urls_scrapped(u)
         scraping = results[0]
         urls_processed = results[1]
@@ -54,19 +55,19 @@ if st.sidebar.checkbox('Run scrapping'):
         st.markdown('Do you want to save?')
         if st.button("Yes!"):
             json__saving(scraping)
-            
+
 
 elif st.sidebar.checkbox('Benchmark visualisation'):
-    
+
     path_to_json = 'scrapings/'
-        
+
     dataframes = {}
     requests_saved = []
     max_deepness = []
 
     files_name = os.listdir(path_to_json)
 
-    '''for file in files_name:
+    for file in files_name:
         full_path = os.path.join(path_to_json, file)
         if os.path.isfile(full_path):
             with open(full_path, 'r') as myfile:
@@ -88,18 +89,103 @@ elif st.sidebar.checkbox('Benchmark visualisation'):
                             requests += f"['{branch}']"
                         result = eval(req)
                         requests_saved.append(requests)
-                        
+
                         variable_name = f"df_{file}_{req}"
-                        
+
                         dataframes[variable_name] = pd.DataFrame(result)
-                        
+
+    requests_saved_cleaned = list(set(requests_saved))
+    requests_saved_cleaned_list = [chain_to_list(
+        request) for request in requests_saved_cleaned]
+    # ICI JE DOIS REGARDER LA LONGEUR DE TOUTE LES LISTES ET CREE OPTION I DE LA TAILLE DE LA PLUS ONGUE LISTE
+
+    my_final_request = []
+
     # Create a dropdown menu with selectbox method
-    option = st.selectbox(
+    first_layer = []
+    for req in requests_saved_cleaned_list:
+        first_layer.append(req[0])
+    first_layer = set(first_layer)
+
+    option1 = st.selectbox(
         'Choose an option',
-        ('Option 1', 'Option 2', 'Option 3')
+        (first_layer)
     )
 
-    # Display the selected option
+    my_final_request.append(option1)
+
+    second_layer = []
+    for req in requests_saved_cleaned_list:
+        if req[0] == option1:
+            second_layer.append(req[1])
+    second_layer = set(second_layer)
+    option2 = st.selectbox(
+        'Choose an option',
+        (second_layer)
+    )
+    my_final_request.append(option2)
+
+    third_layer = []
+    for req in requests_saved_cleaned_list:
+        if req[0] == "Circular Light Bulbs & Lamps":
+            if req[1] == "Circular Lamps":
+                third_layer.append(req[2])
+    third_layer = set(third_layer)
+    option3 = st.selectbox(
+        'Choose an option',
+        (third_layer)
+    )
+    my_final_request.append(option3)
+
+    my_final_request_chain = list_to_chain(my_final_request)
+
+    modele_regex = r"df_\d{4}-\d{2}-\d{2}_mon_fichier\.json_data" + \
+        re.escape(my_final_request_chain)
+
+    cles_trouvees = []
+
+    for cle in dataframes.keys():
+
+        if re.match(modele_regex, cle):
+            cles_trouvees.append(cle)
+
+    print("Clés correspondantes au modèle regex :")
+    dataframes_to_concat = []
+    for cle_trouvee in cles_trouvees:
+        print(cle_trouvee)
+        dataframes_to_concat.append(dataframes[cle_trouvee])
+
+    df_concatene = pd.concat(dataframes_to_concat, ignore_index=True)
+
+    # In order to thing the same items, I attribute them a Ref number. If the items is the same, everything but price and date has to be the same
+    colonnes_a_exclure = ['Date', 'Price']
+
+    df_concatene['Ref'] = df_concatene.groupby(
+        [colonne for colonne in df_concatene.columns if colonne not in colonnes_a_exclure]).ngroup()
+    df_concatene.sort_values(by=['Ref'], inplace=True)
+
+    fig, axes = plt.subplots(
+        nrows=(max(df_concatene['Ref']) // 2) + 1, ncols=2, figsize=(15, 15))
+    fig.suptitle(my_final_request_chain.replace("[", "").replace(
+        "]", "").replace("''", " ").replace("'", ""), fontsize=16)
+    for i, ax in enumerate(axes.flat):
+        if i < max(df_concatene['Ref']):
+            df = df_concatene[df_concatene['Ref'] == i]
+            ax.plot(df['Date'], df['Price'], color='blue', linestyle='-')
+            ax.scatter(x=df['Date'], y=df['Price'])
+            ax.set_xlabel('Date')
+            ax.set_ylabel('Price')
+
+            first_row_values = df_concatene.drop(
+                columns=['Date', 'Price']).iloc[0].values
+            string_first_row = ', '.join(map(str, first_row_values))
+            ax.set_title(string_first_row, fontsize=11)
+
+    plt.tight_layout()
+
+    st.pyplot(fig)
+
+    '''# Display the selected option
     st.write('You selected:', option)
                         
     # Create lists to store data for each level
--- web_app_functions.py
+++ web_app_functions.py
@@ -11,125 +11,136 @@ import os
 import re
 import datetime
 
-#Function to scrap one page
-def url_scraper (url) :
-    
-    #Opening web browser
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Function to scrap one page
+
+
+def url_scraper(url):
+
+    # Opening web browser
     options = Options()
     options.add_argument("--start-maximized")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver = webdriver.Chrome(service=Service(
+        ChromeDriverManager().install()), options=options)
 
-    #Getting the the page we want to scrap
+    # Getting the the page we want to scrap
     driver.get(url)
     driver.implicitly_wait(1)
 
-    #Extracting page main title
+    # Extracting page main title
     title_page = driver.find_element(By.CLASS_NAME, 'KkejIK').text
-    #Identification of each sections of the page. Creating an empty dictionary to save the data.
+    # Identification of each sections of the page. Creating an empty dictionary to save the data.
     sections = driver.find_elements(By.CLASS_NAME, 'MAcbb-')
     section_dict = {}
 
-    #Looping throught each section to get the information we want which are the tables
-    for section in sections :
-        #Extracting section title
+    # Looping throught each section to get the information we want which are the tables
+    for section in sections:
+        # Extracting section title
         title_section = section.find_element(By.CLASS_NAME, "sC0Aof").text
         sub_section_dict = {}
         section_dict[title_section] = sub_section_dict
-        
-        #Looping throught ever sub-section to extract every table
-        if section.find_elements(By.CLASS_NAME, "T8G8vu") : 
+
+        # Looping throught ever sub-section to extract every table
+        if section.find_elements(By.CLASS_NAME, "T8G8vu"):
             sub_sections = section.find_elements(By.CLASS_NAME, "T8G8vu")
             extra_title = ''
             for sub_section in sub_sections:
-                
+
                 try:
-                    title_sub_section = extra_title + sub_section.find_element(By.CLASS_NAME, 'SQoGqa').text
+                    title_sub_section = extra_title + \
+                        sub_section.find_element(By.CLASS_NAME, 'SQoGqa').text
                     sub_section_dict[title_sub_section] = {}
-                    
-                    #To load the HTML, we need to 'activate' the JS by clicking on a button of the table
+
+                    # To load the HTML, we need to 'activate' the JS by clicking on a button of the table
                     sub_section.find_element(By.CLASS_NAME, 'JxT10f').click()
-                    
-                    #Getting features name as they are different on each urls. Features comes in one long str thet needs to be split
-                    features = sub_section.find_element(By.CLASS_NAME, 'JxT10f')
+
+                    # Getting features name as they are different on each urls. Features comes in one long str thet needs to be split
+                    features = sub_section.find_element(
+                        By.CLASS_NAME, 'JxT10f')
                     feature_list = (features.text).split('\n')
-                    
-                    #Getting the table associated with the sub section
+
+                    # Getting the table associated with the sub section
                     tables = sub_section.find_element(By.CLASS_NAME, 'cjpIYY')
                     feature_dict = {}
-                    
-                    #Adding the date of scrapping
+
+                    # Adding the date of scrapping
                     current_date = datetime.datetime.now().strftime("%Y-%m-%d")
-                    
-                    #Going through every row of every column and creating the list of values associated with each feature
+
+                    # Going through every row of every column and creating the list of values associated with each feature
                     rows = tables.find_elements(By.TAG_NAME, 'tr')
                     for i in range(len(feature_list)):
                         feature_dict[feature_list[i]] = []
                         feature_dict['Date'] = []
-                        
+
                         for row in rows:
-                            value = row.find_element(By.XPATH, f'./td[{i+1}]').text
+                            value = row.find_element(
+                                By.XPATH, f'./td[{i+1}]').text
                             feature_dict[feature_list[i]].append(value)
                             feature_dict['Date'].append(current_date)
-                            
+
                     sub_section_dict[title_sub_section] = feature_dict
-                    
+
                 except Exception as e:
-                    extra_title = sub_section.find_element(By.CLASS_NAME, 'SQoGqa').text + ' '
+                    extra_title = sub_section.find_element(
+                        By.CLASS_NAME, 'SQoGqa').text + ' '
                     del sub_section_dict[title_sub_section]
 
-        else :
-            
+        else:
+
             sub_section.find_element(By.CLASS_NAME, 'JxT10f').click()
-        
-            #Getting features name as they are different on each urls. Features comes in one long str thet needs to be split
+
+            # Getting features name as they are different on each urls. Features comes in one long str thet needs to be split
             features = sub_section.find_element(By.CLASS_NAME, 'JxT10f')
             feature_list = (features.text).split('\n')
-            
-            #Getting the table associated with the sub section
+
+            # Getting the table associated with the sub section
             tables = sub_section.find_element(By.CLASS_NAME, 'cjpIYY')
             feature_dict = {}
-            
-            #Adding the date of scrapping
+
+            # Adding the date of scrapping
             current_date = datetime.datetime.now().strftime("%Y-%m-%d")
-            
-            #Going through every row of every column and creating the list of values associated with each feature
+
+            # Going through every row of every column and creating the list of values associated with each feature
             rows = tables.find_elements(By.TAG_NAME, 'tr')
             for i in range(len(feature_list)):
                 feature_dict[feature_list[i]] = []
                 feature_dict['Date'] = []
-                
+
                 for row in rows:
                     value = row.find_element(By.XPATH, f'./td[{i+1}]').text
                     feature_dict[feature_list[i]].append(value)
                     feature_dict['Date'].append(current_date)
-                    
+
             sub_section_dict[title_sub_section] = feature_dict
 
     page_dict = {}
-    page_dict[title_page] = section_dict 
+    page_dict[title_page] = section_dict
 
     driver.quit()
-    
+
     return page_dict
 
 
-#Function to scrap all the urls and follow the one we have scrapped or not
-def urls_scrapped(urls) :
+# Function to scrap all the urls and follow the one we have scrapped or not
+def urls_scrapped(urls):
     data = []
-    urls_done = [] #urls_done has been created if the script come to a stop for whatever reason. We can look at the last link added here and restart from here.
-    for url in urls :
-        print(url) #Help to follow the script live
+    # urls_done has been created if the script come to a stop for whatever reason. We can look at the last link added here and restart from here.
+    urls_done = []
+    for url in urls:
+        print(url)  # Help to follow the script live
         try:
             scraping = url_scraper(url)
             data.append(scraping)
             urls_done.append(url)
-        except: #If there is an issue on a url we can just skip it and move on
+        except:  # If there is an issue on a url we can just skip it and move on
             pass
 
     return data, urls_done[-1]
 
 
-#Function to save the scraping
+# Function to save the scraping
 def json__saving(page_dict):
     json_string = json.dumps(page_dict)
     current_date = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -137,13 +148,15 @@ def json__saving(page_dict):
         datas.write(json_string)
 
 
-#Function to find all the branches of the json
+# Function to find all the branches of the json
 def find_branches(dictionary, depth, max_depth, parent_key=None):
     branches = []
     for key, value in dictionary.items():
-        current_key = key if parent_key is None else f"{parent_key}\\{key}"  # Change '.' to '\\'
+        current_key = key if parent_key is None else f"{
+            parent_key}\\{key}"  # Change '.' to '\\'
         if isinstance(value, dict) and depth < max_depth:
-            branches.extend(find_branches(value, depth + 1, max_depth, current_key))
+            branches.extend(find_branches(
+                value, depth + 1, max_depth, current_key))
         else:
             branches.append(current_key)
     return branches
@@ -157,3 +170,13 @@ def find_nested_depth(dictionary, current_depth=0):
             depth = find_nested_depth(value, current_depth + 1)
             max_depth = max(max_depth, depth)
     return max_depth
+
+
+def chain_to_list(chaine):
+    return re.findall(r"\['(.*?)'\]", chaine)
+
+
+def list_to_chain(liste):
+    elements = [f"['{element}']" for element in liste]
+    chain = ''.join(elements)
+    return chain
